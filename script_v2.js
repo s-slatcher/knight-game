bc97f9b19c3d1e375239a2ba4ac4562eba41f8a4
Selkie Slatcher <selkieslatcher@gmail.com>
2023-07-03 19:42:33 -0400

    reading keyboard input

--- script_v2.js
+++ script_v2.js
@@ -1,15 +1,15 @@
 window.addEventListener('load', function(){
 
-
 const keyRecord = [];
 window.addEventListener('keydown', (e) => {
     if (keyRecord.indexOf(e.key) === -1) keyRecord.push(e.key)
     if (e.key === ' ') e.preventDefault();
-    console.log(keyRecord)
 })
 window.addEventListener('keyup', (e) => {
     keyRecord.splice(keyRecord.indexOf(e.key),1)
 })
+
+window.addEventListener('blur', () => keyRecord.splice(0,keyRecord.length))
 
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext('2d');
@@ -39,7 +39,10 @@ class Game{
         }
     }
     draw(){
+        
         this.ctx.fillRect(this.testX,this.height/2,50,50);
+        this.player.draw();
+        
     }
     getFramesDue(timestamp){
         let frameTime = timestamp - this.lastTimeStamp;
@@ -55,10 +58,72 @@ class Player{
     constructor(game){
         this.game = game;
         this.spritesLoaded = false;
-        this.fetchSprites()
-    
+        this.lastWASDInput = 'none'
+        this.attackInput;
+        this.#fetchSprites()
+            .then(() => {
+                this.block = new Block(this.game, this.blockSprite)
+                this.attack = new Attack(this.game, this.attackSprite)
+            })
     }
-    async fetchSprites(){
+    
+    readInput(keyRecord){
+        let newWASDInput = this.lastWASDInput;
+        if (keyRecord.length === 0) newWASDInput = 'none'
+        else if (!keyRecord.includes(this.lastWASDInput)){
+            if (keyRecord.includes('a')) newWASDInput = 'a'
+            if (keyRecord.includes('d')) newWASDInput = 'd'
+        }
+        this.lastWASDInput = newWASDInput;
+
+        if(keyRecord.includes(" ")) this.attackInput = this.lastWASDInput
+        else this.attackInput = 'none'
+        
+        
+
+        
+
+        // read the key record, detect if a movement key was pressed, detect if new inputs are priotized or not
+        // send current movement value to Blocking class object, it will create/run through a queue of frames
+        // Player.update can also tell block to fade in/out depending on 
+
+        // let inputCandidate = 'neutral';
+        // if (keyRecord.d.pressed) inputCandidate = 'd'
+        // if (keyRecord.a.pressed) inputCandidate = 'a'
+        // this.input = this.lastInput === 'neutral' || !keyRecord[this.lastInput].pressed ?
+        //     inputCandidate : this.lastInput;
+
+
+        // if (this.inputDelayCounter > 0) {
+        //     this.input = this.lastInput
+        //     this.inputDelayCounter -= 1;
+        // }
+        // if (this.input !== this.lastInput) {
+        //     this.inputDelayCounter = 4;
+        // }
+        // this.lastInput = this.input;
+
+        // if (keyRecord[' '].pressed && this.inputDelayCounter === 0) {
+        //     Object.values(this.blockDirection).forEach((e) => e = false)
+        //     this.inputDelayCounter = this.attackSprite.frames.length
+        //     this.isAttacking = true;
+        // }
+    }
+    update(keyRecord){
+        if(!this.spritesLoaded) return;
+        this.block.update();
+        let input = this.readInput(keyRecord);
+
+    }
+    draw(){
+        if (!this.spritesLoaded) return;
+        this.block.draw();
+    }
+        
+        //update different player states based on 
+    
+    fadeOut(){}
+    async #fetchSprites(){
         const [atkResponse, blockResponse] = await Promise.all([
             fetch("./sword-attack-1.json"),
             fetch("./sword48.json")])
@@ -72,66 +137,54 @@ class Player{
         })
         this.spritesLoaded = true;
     }
-    readInput(keyRecord){
-        return keyRecord
-    }
-    update(keyRecord){
-        if(!this.spritesLoaded) return;
-        let input = this.readInput(keyRecord);
-        
-        
-        
-    }
-
-        
-        //update different player states based on 
-    
-    attacking(direction){
-       
-    }
-    blocking(direction){
-        if(!direction){
-            this.fadeOut()
-        }
-    }
-    fadeOut(){}
 }
 
-// class Attacking extends Player{}
+class Block{
+    constructor(game, sprite){
+        this.game = game
+        this.ctx = game.ctx
+        this.offSetWidth = 0 + game.width * 0.1;
+        this.offSetHeight = 0 + game.height * 0.15;
+        this.sprite = sprite;
+        this.framesArr = sprite.frames
+        this.frame = 18;
+        this.frameIncrement = 1;
+    }
+    update(){
+        this.frame += this.frameIncrement;
+        if (this.frame === this.framesArr.length || this.frame < 0) {
+            this.frameIncrement *= -1;
+            this.frame += this.frameIncrement
+        }
+    }
+    draw(){
+        let frame = this.framesArr[this.frame].frame
+        ctx.drawImage(this.sprite.image, frame.x, frame.y,
+            frame.w, frame.h,
+            this.offSetWidth,
+            this.offSetHeight,
+            frame.w, frame.h)
+        
+    }
+}
 
-// class Blocking extends Player{}
+class Attack{}
+
+
 
 // class Backgrounds extends Game{}
 
 // class Enemies extends Game{}
 
+let game = new Game(ctx, CANVAS_WIDTH, CANVAS_HEIGHT)
+animate(0);
 
-function animate(timestamp, game){
+function animate(timestamp){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     game.update(timestamp, keyRecord)
     game.draw();
-    requestAnimationFrame(function(timestamp){
-        animate(timestamp, game)
-    });
+    requestAnimationFrame(animate)
 }
 
-(async () => {
-    const res = await Promise.all([
-        fetch("./sword48.json"),
-        fetch("./sword-attack-1.json"),
-        //fetch("./crossbow-attack.json")
-    ]);
-    const data = await Promise.all(res.map(r => r.json()))
-    let game = new Game(ctx, CANVAS_WIDTH, CANVAS_HEIGHT)
-    console.log(data.flat())
-    data.forEach((json) => {
-        json.image = new Image();
-        json.image.src = `./${json.meta.image}`;
-        json.offSetWidth = 0 + CANVAS_WIDTH * 0.1;
-        json.offSetHeight = 0 + CANVAS_HEIGHT * 0.15;
-        
-    })    
-    animate(0, game);
-})();
 
 });
